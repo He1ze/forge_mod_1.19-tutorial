13fca2a688dc20f32c38ac663b0f774471eb5414
He1ze <87829320+He1ze@users.noreply.github.com>
2022-11-12 19:37:37 -0500

    made a comment

--- src/main/java/net/jp4/tutorialmod/TutorialMod.java
+++ src/main/java/net/jp4/tutorialmod/TutorialMod.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-//TODO: SAVE YOUR CHANGES type ./gradlew build in the terminal.
+//TODO: SAVE YOUR CHANGES type ./gradlew build in the terminal. Good Luck!
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TutorialMod.MOD_ID)
